cf4669252fb7f7b2a0f3979857d900e77a0041c9
Martin Novoa <martinnovoar2@gmail.com>
2026-05-17 15:24:54 -0300

    rafactor login and signup

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -6,10 +6,11 @@ import './index.css'
 import './styles/shell.css'
 import './styles/components.css'
 import App from './App.tsx'
+import {esUY} from "@clerk/localizations";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ClerkProvider publishableKey={import.meta.env.VITE_CLERK_PUBLISHABLE_KEY}>
+    <ClerkProvider publishableKey={import.meta.env.VITE_CLERK_PUBLISHABLE_KEY} localization={esUY} >
       <BrowserRouter>
         <App />
       </BrowserRouter>
